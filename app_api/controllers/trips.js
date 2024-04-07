3ebc86601a442f9de28f0892b63b3662de9a3cc4
Anthony D'Angelo <tony.dangelo87@gmail.com>
2024-04-06 20:16:06 -0400

    Adding api endpoints and db connection

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Trip = require("../models/travlr"); // Register model
+const Model = mongoose.model("trips");
+
+// GET: /trips - lists of all the trips
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsList = async (req, res) => {
+  const q = await Model.find({}).exec();
+
+  // Uncomment the following line to show results of query
+  // on the console
+  // console.log(q);
+
+  if (!q) {
+    return res.status(404).json(err);
+  } else {
+    return res.status(200).json(q);
+  }
+};
+
+// GET: /trips/:tripCode - lists a single trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsFindByCode = async (req, res) => {
+  const q = await Model.find({ code: req.params.tripCode }).exec();
+
+  // Uncomment the following line to show results of query
+  // on the console
+  // console.log(q);
+
+  if (!q) {
+    return res.status(404).json(err);
+  } else {
+    return res.status(200).json(q);
+  }
+};
+
+module.exports = { tripsList, tripsFindByCode };
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,32 @@
-var fs = require("fs");
-var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
-
+// var fs = require("fs");
+// var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
+const tripsEndpoint = "http://localhost:3000/api/trips";
+const options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
+};
 /* GET travel view */
-const travel = (req, res) => {
-  res.render("travel", { title: "Travlr Getaways", trips });
+const travel = async function (req, res) {
+  // console.log('TRAVEL CONTROLLER BEGIN');
+  await fetch(tripsEndpoint, options)
+    .then((res) => res.json())
+    .then((json) => {
+      // console.log(json)
+      let message = null;
+      if (!(json instanceof Array)) {
+        message = "API Lookup Error";
+        json = [];
+      } else {
+        if (!json.length) {
+          message = "No trips exist in our database!";
+        }
+      }
+      res.render("travel", { title: "Travlr Getaways", trips: json });
+    })
+    .catch((err) => res.status(500).send(e.message));
+  // console.log('TRAVEL CONTROLLER AFTER RENDER');
 };
 
 module.exports = {
