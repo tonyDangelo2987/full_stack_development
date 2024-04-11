6e55dfa22d02143849505bfdfa1be93ff79a6ea5
Anthony D'Angelo <tony.dangelo87@gmail.com>
2024-04-11 18:54:46 -0400

    Adding card component

---
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './trip-card.component.html',
+  styleUrl: './trip-card.component.css',
+})
+export class TripCardComponent implements OnInit {
+  @Input('trip') trip: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
