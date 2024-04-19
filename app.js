c3c492d8be1e9613b0d395f245d3854b43af1888
Anthony D'Angelo <tony.dangelo87@gmail.com>
2024-04-18 20:22:48 -0400

    Adding some authentication

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const passport = require("passport");
 
 // Define routers
 var indexRouter = require("./app_server/routes/index");
@@ -15,6 +18,8 @@ var handlebars = require("hbs");
 
 // Bring in the database
 require("./app_api/models/db");
+
+require("./app_api/config/passport");
 
 var app = express();
 
@@ -31,16 +36,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
 
 // Enable CORS
 app.use("/api", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
+});
+
+// Catch unauthorized error and create 401
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ message: err.name + ": " + err.message });
+  }
 });
 
 // Wire-up routes to controllers
